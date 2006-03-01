dd229be8e651b6272ed3734dda17da78e0eb0bb3
Artem Baguinski <artm@v2.nl>
2006-03-01 18:05:54 +0000

    tidy macos stuff again

--- src/main.cpp
+++ src/main.cpp
@@ -206,13 +206,16 @@ void inner_main(int argc, char **argv)
 int main(int argc, char *argv[])
 {
 #ifdef __APPLE__
-        // for mac osx - get cwd and add guile_scripts to the GUILE_LOAD_PATH env var.
+        // make sure guile can boot on darwin
         std::string argv0(argv[0]);
-        unsigned int lastpos = argv0.rfind('/', argv0.length());
+        unsigned int lastpos = argv0.rfind('/');
+        lastpos = argv0.rfind('/', lastpos-1); // skip "MacOS"
         if ( lastpos!=std::string::npos )
         {
-                std::string guile_load_path = argv0.substr(0,lastpos)+std::string("/guile_scripts");
-                putenv( const_cast<char*>((std::string("GUILE_LOAD_PATH=")+guile_load_path).c_str() ) );
+                std::string guile_load_path = argv0.substr(0,lastpos) +
+                        std::string("/Resources/guile_scripts");
+                putenv( const_cast<char*>((std::string("GUILE_LOAD_PATH=") +
+                                           guile_load_path).c_str() ) );
         }
 #endif
 
